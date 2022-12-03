c8172a439e1f696aebca51b649f099a253719ab3
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2022-12-03 18:07:48 -0500

    added ids back to radios to match labels, set up submit fuction to check for correct answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,15 +10,22 @@
 // WHEN the game is over
 // THEN I can save my initials and my score
 
-var form = document.querySelector('form');
+var form = document.forms.codeQuiz;
+var radio1 = form.elements.firstQ;
+var value1 = radio1.value;
+var radio2 = form.elements.secondQ;
+var value2 = radio2.value;
+var radio3 = form.elements.thirdQ;
+var value3 = radio3.value
+var radio4 = form.elements.fourthQ;
+var value4 = radio4.value;
+var radio5 = form.elements.fifthQ;
+var value5 = radio5.value;
 var nextEl = document.querySelector('.advance');
 var boxes = document.querySelectorAll('.box');
-var answers = ["choice1", "choice2", "choice3", "choice4", "choice1"];
+var answers = ["a", "b", "c", "d", "a"];
 var cursor = 0;
-
-console.log(boxes[1].dataset.index);
-console.log(boxes)
-
+var score = 0;
 // var userScore = localStorage.getItem("userScore");
 
 var displayBox = function () {
@@ -40,14 +47,23 @@ var advance = function () {
 
 nextEl.addEventListener('click', advance)
 
-// form.addEventListener('submit', function (event) {
-//     event.preventDefault();
-//     var el = event.target;
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var checked1 = document.querySelector('input[name="firstQ"]:checked').value;
+    var checked2 = document.querySelector('input[name="secondQ"]:checked').value;
+    var checked3 = document.querySelector('input[name="thirdQ"]:checked').value;
+    var checked4 = document.querySelector('input[name="fourthQ"]:checked').value;
+    var checked5 = document.querySelector('input[name="fifthQ"]:checked').value;
+    var chex = [checked1, checked2, checked3, checked4, checked5];
+    console.log(chex);
+    for (var i = 0; i < chex.length; i++) {
+        if (chex[i] === answers[i]) {
+            score++
+        }
+    }
+    console.log("score: " + score)
 
-//     if (el.matches('#choice')) {
-
-//     }
-// });
+});
 
 displayBox();
 
